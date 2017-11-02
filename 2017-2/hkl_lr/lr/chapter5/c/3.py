372aebe061252a357665faaccc2e95f29c1f41bc
canyousee <940520455@qq.com>
2017-11-02 20:28:06 +0800

    实验二报告提交

---
+++ 2017-2/hkl_lr/lr/chapter5/c/3.py
@@ -0,0 +1,19 @@
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
+dst_ip="10.0.2.5"
+src_port=RandShort()
+dst_port=80
+
+xmas_resp=sr1(IP(dst=dst_ip)/TCP(dport=dst_port,flags="FPU"),timeout=10)
+
+
+if(str(type(xmas_resp))=="<type 'NoneType'>"):
+    print "Open|Filtered"
+elif(xmas_resp.haslayer(TCP)):
+    if(xmas_resp.getlayer(TCP).flags==0x14):
+        print "Closed"
+    elif(xmas_resp.haslayer(ICMP)):
+        if(int(xmas_resp.getlayer(ICMP).type)==3 and int(xmas_resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
+            print "Filtered"
+
