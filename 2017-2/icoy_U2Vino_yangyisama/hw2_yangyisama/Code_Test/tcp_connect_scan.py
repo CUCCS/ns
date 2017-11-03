de04ee793eed62f44a759d862d7e521221fea764
yangyisama <wlh.471408730@qq.com>
2017-11-03 23:43:13 +0800

    Create tcp_connect_scan.py
    添加了tcp_connect_scan的代码

---
+++ 2017-2/icoy_U2Vino_yangyisama/hw2_yangyisama/Code_Test/tcp_connect_scan.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
+
+dst_ip=sys.argv[1] if len(sys.argv)>1 else '10.0.4.254'
+src_port =RandShort()
+dst_port=int (sys.argv[2]) if len(sys.argv)>2 else 80
+
+tcp_connect_scan_resp = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=10)
+if(str(type(tcp_connect_scan_resp))=="<type 'NoneType'>"):
+    print('Port %d Filtered' % dst_port)
+elif(tcp_connect_scan_resp.haslayer(TCP)):
+    if(tcp_connect_scan_resp.getlayer(TCP).flags == 0x12):
+        send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="AR"),timeout=10)
+        print('Port %d Open' % dst_port)
+    elif (tcp_connect_scan_resp.getlayer(TCP).flags == 0x14):
+        print('Port %d Closed' % dst_port) 
